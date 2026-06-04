5a1080371d191b4188d7b36cf9068489de657276
JIMIN <bagj5082@gmail.com>
2026-06-04 17:55:43 +0900

    굿

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,83 @@ import java.util.Scanner;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static <and> void main(String[] args) {
+//
+//    int a = 5;
+//    int b = 3;
+//    int c = a - b;
+//        System.out.println(c);
 
-    int a = 5;
-    int b = 3;
-    int c = a - b;
-        System.out.println(c);
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("이름을 입력하세요: " );
+//        String name = input.nextLine();
+//        System.out.print("나이를 입력하세요: ");
+//        String age = input.nextLine();
+//        System.out.println("출력결과");
+//        System.out.println("이름: "+ name + "\n나이: " + age);
+
+
+//        int a = 5;
+//        int b = 3;
+//
+//        int pl = a + b;
+//        System.out.println("더하기결과: " + pl);
+//        int m = a - b;
+//        System.out.println("뺴기결과: " + m);
+//        int n = a * b;
+//        System.out.println("곱하기결과: " + n);
+//        int u = a / b;
+//        System.out.println("나누기결과: " + u);
+//        int p = a % b;
+//        System.out.println("나머지결과: " + p);
+//
+////
+//
+//        Scanner light = new Scanner(System.in);
+//        System.out.print("신호등 색깔을 입력하세요:");
+//         String g = light.next();
+//
+//        if (g.equals("초록불")){
+//            System.out.println("건너세요");
+//        }else if (g.equals("노란불")){
+//            System.out.println("주의 하세요");
+//        }else if (g.equals("빨간불")){
+//            System.out.println("멈추세요");
+//        } else{
+//            System.out.println("올바른 색을 입력하세요");
+//        }
+//
+//        Scanner number = new Scanner(System.in);
+//        int c = 1 + 1;
+//
+//        System.out.print("올바른 숫자를 입력하세요: ");
+//        int a = number.nextInt();
+//
+//        if (a == c ){
+//            System.out.println("정답 입니다.");
+//        }else if (a != c ){
+//            System.out.println("정답이 아닙니다.");
+//        }
+//
+        int b = 5;
+        b +=3;
+        System.out.println(b);
+        b -=2;
+        System.out.println(b);
+        // == , != , < , > , <= , >=
+
+        System.out.println("++b:" + ++b);
+
+
+
+        System.out.println("b++:" + b++);
+        System.out.println(b);
+
 
 
     }
+
 }
 
 
