b6039e83d68a80ce945dbb7c954ff49d2a55489f
JIMIN <bagj5082@gmail.com>
2026-06-04 10:50:37 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static <and> void main(String[] args) {
+
+    int a = 5;
+    int b = 3;
+    int c = a - b;
+        System.out.println(c);
+
+
+    }
+}
+
+
+
+
---
+++ src/g1/Main.java
@@ -0,0 +1,25 @@
+package g1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("===학생 정보===");
+
+        String name = "박지민";
+        System.out.println("name = " + name);
+        int age = 24;
+        System.out.println("age = " + age);
+        double height = 177.2;
+        System.out.println("height = " + height);
+        char grade = '2';
+        System.out.println("grade = " + grade);
+        boolean isStudent = false;
+        System.out.println("isStudent = " + isStudent);
+
+
+
+
+
+
+    }
+}
