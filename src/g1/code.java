6dc263282af633f67bd75698cf290a69c8daa23a
JIMIN <bagj5082@gmail.com>
2026-06-08 09:11:44 +0900

    수정

--- src/g1/code.java
+++ src/g1/code.java
@@ -4,8 +4,11 @@ public class code {
     public static void main(String[] args) {
         int a = 7;
         int b = 3;
+//
+//        int  c = a * b;
+//        System.out.println(c);
 
-        int  c = a * b;
+        int c = a % b;
         System.out.println(c);
     }
 }
