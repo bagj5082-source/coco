e0f53fd0ccb17564c8a8570ef11d9be3589099bb
JIMIN <bagj5082@gmail.com>
2026-06-10 09:45:44 +0900

    codekata 수정

--- src/g1/codekata.java
+++ src/g1/codekata.java
@@ -16,18 +16,36 @@ public class codekata {
 //        System.out.println(c);
        
        
-            int age; 
-            int answer = 0;
-            int year = 2022;
-            age = 40;
-            answer = year - age + 1;
-        System.out.println(answer);
+//            int age;
+//            int answer = 0;
+//            int year = 2022;
+//            age = 40;
+//            answer = year - age + 1;
+//        System.out.println(answer);
+                int sol;
+                int num1 = 3;
+                int num2 = 3;
+
+                if(num1 == num2){
+                    System.out.println("1");
+                } else {
+                    System.out.println("-1");
+                }
+
+                //dldldl
+
+
+
+    }
+
+
 
             
-        }
+
+}
 
 
-        }
+
 
         
             
